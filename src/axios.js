0a3da4c137ab21501b8bdcfb563f459538801fc1
Solium Dev <lucas.develop.prog@gmail.com>
2022-06-23 15:07:18 -0300

    :book::tada: Intercepting request with Axios.

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,30 @@
 import axios from "axios";
 import { api } from "./config/config";
 
-export default axios.create({
+const instance = axios.create({
   baseURL: api.baseURL,
 });
+
+instance.interceptors.request.use(
+  (config) => {
+    console.log("Interceptando requisição: ", config);
+    config.data = {
+      ...config.data,
+      curso: "Vue JS",
+    };
+    //   return config;
+    return new Promise((resolve) => {
+      console.log("Fazendo requisição aguardar");
+      setTimeout(() => {
+        console.log("Enviando requisição");
+        resolve(config);
+      }, 2000);
+    });
+  },
+  (error) => {
+    console.log("Erro ao fazer requisição: ", error);
+    return error;
+  }
+);
+
+export default instance;
