ba0b092817ea7b9f582dbc304958079acf21e1b2
Solium Dev <lucas.develop.prog@gmail.com>
2022-06-24 10:01:50 -0300

    :book::tada: Setting common and specifical headers in request interceptor and creating our own header.

--- src/axios.js
+++ src/axios.js
@@ -12,18 +12,35 @@ instance.interceptors.request.use(
       ...config.data,
       curso: "Vue JS",
     };
-    //   return config;
-    return new Promise((resolve) => {
-      console.log("Fazendo requisição aguardar");
-      setTimeout(() => {
-        console.log("Enviando requisição");
-        resolve(config);
-      }, 2000);
-    });
+    config.headers.common["Authorization"] = `Bearer token_jwt`;
+    // Definindo header para tipos de requisição específicos.
+    config.headers.put["Meu-Cabecalho"] = "CursoVueJS";
+    return config;
+    // return new Promise((resolve) => {
+    //   console.log("Fazendo requisição aguardar");
+    //   setTimeout(() => {
+    //     console.log("Enviando requisição");
+    //     resolve(config);
+    //   }, 2000);
+    // });
   },
   (error) => {
     console.log("Erro ao fazer requisição: ", error);
     return error;
+  }
+);
+
+instance.interceptors.response.use(
+  (res) => {
+    console.log("Interceptando resposta", res);
+    if (Array.isArray(res.data)) {
+      res.data = res.data.slice(1, 3);
+    }
+    return res;
+  },
+  (error) => {
+    console.log("Erro capturado no interceptador da respostas", error);
+    return Promise.reject(error);
   }
 );
 
