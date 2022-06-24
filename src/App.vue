b5e0913b335fbf9be131981d87297bdad03d6599
Solium Dev <lucas.develop.prog@gmail.com>
2022-06-24 10:22:00 -0300

    :book::tada: Using Axios.all function to resolve multiple and simultaneous request. Plus: ES6 feature to make easier the way we handle the response return from all requests.

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,48 @@
 <template>
   <div id="app">
-    
     <div class="jumbotron jumbotron-fluid">
       <div class="container">
         <h1 class="display-4">Requisições HTTP no Vue</h1>
-        <p class="lead">Usando a biblioteca Axios para fazer chamadas Ajax à uma API REST.</p>
+        <p class="lead">
+          Usando a biblioteca Axios para fazer chamadas Ajax à uma API REST.
+        </p>
       </div>
     </div>
 
     <div class="container">
-
       <TarefasLista />
-
     </div>
-
   </div>
 </template>
 
 <script>
-
-import TarefasLista from './components/TarefasLista.vue'
+import axios from "axios";
+import { api } from "./config/config";
+import TarefasLista from "./components/TarefasLista.vue";
 
 export default {
-    components: {
-        TarefasLista
-    }
-}
+  components: {
+    TarefasLista,
+  },
+  created() {
+    axios
+      .all([
+        axios.get(`${api.baseURL}/tarefas/1`),
+        axios.get(`${api.baseURL}/tarefas/3`),
+      ]) //Trabalhando com recursos do ES6
+      .then((res) => {
+        const [tarefa1, tarefa3] = res;
+        console.log("Requisições simultâneas:");
+        console.log("Tarefa 1", tarefa1);
+        console.log("Tarefa 3", tarefa3);
+      });
+    // .then(
+    //   axios.spread((tarefa1, tarefa3) => {
+    // console.log("Requisições simultâneas:");
+    // console.log("Tarefa 1", tarefa1);
+    // console.log("Tarefa 3", tarefa3);
+    //   })
+    // );
+  },
+};
 </script>
